2e9e650ec1d0bfc79c5424e8d003a76676f983bc
Rayllanderson <rayllanderson@gmail.com>
2021-08-03 11:55:03 -0300

    Lidando com erros retornados pela API gRPC

--- src/main/kotlin/br/com/zup/edu/CalculadoraFretesController.kt
+++ src/main/kotlin/br/com/zup/edu/CalculadoraFretesController.kt
@@ -1,8 +1,13 @@
 package br.com.zup.edu
 
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.exceptions.HttpStatusException
 
 @Controller("/api/fretes")
 class CalculadoraFretesController(
@@ -16,9 +21,32 @@ class CalculadoraFretesController(
             .setCep(cep)
             .build()
 
-        val response = grpcClient.calculaFrete(request)
+        try {
 
-        return FreteResponse(cep = response.cep, valor = response.valor)
+            val response = grpcClient.calculaFrete(request)
+            return FreteResponse(cep = response.cep, valor = response.valor)
+
+        } catch (e: StatusRuntimeException) {
+
+            val statusCode = e.status.code
+            val description = e.status.description
+
+            if (statusCode == Status.Code.INVALID_ARGUMENT) {
+                throw HttpStatusException(HttpStatus.BAD_REQUEST, description)
+            }
+
+            if (statusCode == Status.Code.PERMISSION_DENIED) {
+                val statusProto = StatusProto.fromThrowable(e) ?: throw HttpStatusException(HttpStatus.FORBIDDEN, description)
+
+                val anyDetails = statusProto.detailsList[0]
+                val errorDetails = anyDetails.unpack(ErroDetails::class.java)
+
+                throw HttpStatusException(HttpStatus.UNAUTHORIZED, "${errorDetails.code}: ${errorDetails.mensagem}")
+            }
+
+            throw HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.message)
+        }
+
     }
 }
 
